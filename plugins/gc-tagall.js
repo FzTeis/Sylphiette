ed4f038e29e4b8e36718cfac4798b4b2632d2108
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-01 15:05:06 -0600

    Add plugins/gc-tagall.js

---
+++ plugins/gc-tagall.js
@@ -0,0 +1,21 @@
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+  const oi = `*• Mensaje* ${pesan}`;
+  let teks = `\`\`\`  -  ⃟🌅 ⸽⌗⁞ INVOKING GROUP\`\`\`\n\n❏ ${oi}\n\n❏ *Etiquetas:*\n\n`;
+  for (const mem of participants) {
+    teks += ` \`\`\`⁛ @${mem.id.split('@')[0]}\`\`\`\n\n`;
+  }
+  teks += `*𓈒 ˚ ׅׅ□ ׁ⏜⃞🌴𓊪 Sylph is the best*\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
