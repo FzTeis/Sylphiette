6629e78092adf7a050991b404e09baf41aeda09e
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-06 21:44:59 -0600

    Add/Update plugins/econ-mazmorra.js

--- plugins/econ-mazmorra.js
+++ plugins/econ-mazmorra.js
@@ -4,26 +4,26 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   const coinsEncontradas = randomNumber(834, 4930); // Genera una cantidad aleatoria de monedas
   // Añade las monedas al usuario
 
-  const probabilidadMonstruo = randomNumber(1, 100); // Probabilidad de encontrar un monstruo
+  const probabilidadMonstruo = randomNumber(1, 16); // Probabilidad de encontrar un monstruo
 
-  if (probabilidadMonstruo <= 56) { // Probabilidad de 30% de encontrar un monstruo
-    const fuerzaUsuario = randomNumber(10, user.mana); // Fuerza del usuario
+  if (probabilidadMonstruo <= 15) { // Probabilidad de 30% de encontrar un monstruo
+    const fuerzaUsuario = randomNumber(51, 100); // Fuerza del usuario
     const fuerzaMonstruo = randomNumber(50, 90); // Fuerza del monstruo
 
     if (fuerzaUsuario > fuerzaMonstruo) {
       // El usuario mata al monstruo y recibe un bono extra
       const bonoExtra = randomNumber(500, 10000);
-      global.db.data.users[m.sender].coin += bonoExtra;
-      return `> ¡Encontraste un monstruo! Lo derrotaste y encontraste ${coinsEncontradas} monedas más ${bonoExtra} como bono extra.`;
+      global.db.data.users[m.sender].coin += bonoExtra + coinsEncontradas;
+      return `\`\`\`[ 🏆  ¡Encontraste un monstruo! Lo derrotaste y encontraste ${coinsEncontradas} monedas más ➔ ${bonoExtra} como bono extra. ]\`\`\``;
     } else {
       // El monstruo mata al usuario y pierde la recompensa
       global.db.data.users[m.sender].coin -= coinsEncontradas;
-      return `> ¡Encontraste un monstruo! El monstruo te derrotó y perdiste ${coinsEncontradas} monedas.`;
+      return `\`\`\`[ ⚠️  ¡Encontraste un monstruo! El monstruo te derrotó y perdiste ➔ ${coinsEncontradas} monedas. ]\`\`\``;
     
     }
   } else {
     // No se encuentra ningún monstruo
-    return `> ¡Exploraste la mazmorra y encontraste ${coinsEncontradas} monedas!`;
+    return `\`\`\`[ 🎆 ¡Exploraste la mazmorra y encontraste ${coinsEncontradas} monedas! ]\`\`\``;
     global.db.data.users[m.sender].coin += coinsEncontradas; 
   }
 }
