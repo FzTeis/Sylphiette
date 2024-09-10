346f7ef5ddf22d162efa1fb3f005cb87472fa2ca
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-10 15:47:52 -0600

    Add/Update plugins/tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,21 +1,47 @@
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
+import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
 
-let handler = async (m) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `✳️ ${mssg.replyImg}`
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`❖ ${media.length} Byte(s) 
+let handler = async (m, { args, command, usedPrefix }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) throw `✳️ ${mssg.replyImg}`;
+ // if (!args[0]) throw ` \`\`\`[ 🌺 ] Ingresa un texto para guardar la imagen. Ejemplo:\n${usedPrefix + command} Sylph\`\`\``
 
-❖ ${isTele ? '(Sin fecha de caducidad)' : '(Desconocido)'} 
-❖ *URL :* ${link}
-  `)
+  let media = await q.download();
+  let tempFilePath = path.join(os.tmpdir(), 'Sylph');
+  fs.writeFileSync(tempFilePath, media);
+
+  let form = new FormData();
+  form.append('image', fs.createReadStream(tempFilePath));
+
+  try {
+    let response = await axios.post('https://api.imgbb.com/1/upload?key=1f55ea75f24df783643940f3eacbbc96', form, {
+      headers: {
+        ...form.getHeaders()
+      }
+    });
+
+    if (!response.data || !response.data.data || !response.data.data.url) throw '❌ Error al subir el archivo';
+    
+    let link = response.data.data.url;
+    fs.unlinkSync(tempFilePath);
+
+    m.reply(`❖ ${media.length} Byte(s)
+
+❖ (Archivo subido a ImgBB)
+❖ *URL:* ${link}
+    `);
+  } catch (error) {
+    console.error('Error al subir el archivo:', error.message);
+    throw '❌ Error al subir el archivo a ImgBB';
+  }
 }
-handler.help = ['tourl']
-handler.tags = ['tools']
-handler.command = ['upload', 'tourl']
 
-export default handler
+handler.help = ['tourl'];
+handler.tags = ['tools'];
+handler.command = ['upload', 'tourl'];
+
+export default handler;
