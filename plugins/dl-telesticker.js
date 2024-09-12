3a85488620e9e17cedd9db366d52f088e1de717e
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-12 09:20:06 -0600

    Add/Update plugins/dl-telesticker.js

---
+++ plugins/dl-telesticker.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+var handler = async (m, { conn, args }) => {
+	if (args[0] && args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) {
+		let res = await Telesticker(args[0])
+		let limitss = 20
+		await m.reply(`\`\`\`[ 📤 ] Enviando ${res.length} stickers . . .\`\`\``)
+		if (m.isGroup && res.length > limitss) {
+			await m.reply(`\`\`\`[ 🌴 ] El número de stickers es mayor a ${limitss}, para evitar el spam se enviaran a privado\`\`\``)
+			for (let i = 0; i < res.length; i++) {
+				conn.sendMessage(m.sender, { sticker: { url: res[i].url }})
+			}
+		} else {
+			for (let i = 0; i < res.length; i++) {
+				conn.sendMessage(m.chat, { sticker: { url: res[i].url }})
+			}
+		}
+	} else throw `\`\`\`[ 🌃 ] Ingresa una url para descargar.\`\`\``
+}
+handler.help = ['telesticker']
+handler.tags = ['dl']
+handler.command = /^(telestic?ker|stic?kertele)$/i
+handler.limit = true
+
+export default handler
+
+//Thanks Xfarr : https://github.com/xfar05
+async function Telesticker(url) {
+    return new Promise(async (resolve, reject) => {
+        if (!url.match(/(https:\/\/t.me\/addstickers\/)/gi)) throw 'Enther your url telegram sticker'
+        packName = url.replace("https://t.me/addstickers/", "")
+        data = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, {method: "GET",headers: {"User-Agent": "GoogleBot"}})
+        const hasil = []
+        for (let i = 0; i < data.data.result.stickers.length; i++) {
+            fileId = data.data.result.stickers[i].thumb.file_id
+            data2 = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
+            result = {
+            status: 200,
+            author: 'Xfarr05',
+            url: "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + data2.data.result.file_path
+            }
+            hasil.push(result)
+        }
+    resolve(hasil)
+    })
+}
