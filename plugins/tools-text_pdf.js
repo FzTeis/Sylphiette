30e143ff8774229bb818b0d8c7522df59e2084b6
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-21 13:04:20 -0600

    Add/Update plugins/tools-text_pdf.js

---
+++ plugins/tools-text_pdf.js
@@ -0,0 +1,67 @@
+import PDFDocument from 'pdfkit';
+import {
+    Writable
+} from 'stream';
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    // Usage example
+    let query = `\`\`\`[ 🌺 ] Ingresa el texto que quieres convertir a PDF. Ejemplo:\n${usedPrefix + command} Sylphiette - The best\`\`\``
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw query
+    
+    await m.reply(wait)
+    try {
+        let pdf = await textToPDFBuffer(text)
+            .then(buffer => {
+                // Use the resulting buffer as needed
+                return (buffer);
+            })
+        await conn.sendMessage(m.chat, {
+            document: pdf,
+            mimetype: "application/pdf",
+            fileName: `For ${m.name}.pdf`
+        }, {
+            quoted: m
+        })
+        } catch (e) {
+        await m.reply(eror)
+        }
+}
+handler.help = ['texttopdf']
+handler.tags = ['tools']
+handler.command = /^(texttopdf)$/i
+export default handler
+
+async function textToPDFBuffer(text) {
+    return new Promise((resolve, reject) => {
+        const buffers = [];
+        const streamBuffer = new Writable({
+            write(chunk, encoding, next) {
+                buffers.push(chunk);
+                next();
+            },
+        });
+
+        const doc = new PDFDocument();
+
+        doc.pipe(streamBuffer);
+        doc.text(text);
+        doc.end();
+
+        streamBuffer.on('finish', () => {
+            const pdfBuffer = Buffer.concat(buffers);
+            resolve(pdfBuffer);
+        });
+
+        streamBuffer.on('error', reject);
+    });
+}
