1464d9a3e95c6daae4ebf1e9b31d20cb411e3879
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-22 12:01:59 -0600

    Add/Update API-REST/base de dados/functions.js

---
+++ API-REST/base de dados/functions.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch')
+const axios = require('axios')
+
+const getBuffer = async (url, options) => {
+        try {
+                options ? options : {}
+                const res = await axios({
+                        method: "get",
+                        url,
+                        headers: {
+                                'DNT': 1,
+                                'Upgrade-Insecure-Request': 1
+                        },
+                        ...options,
+                        responseType: 'arraybuffer'
+                })
+                return res.data
+        } catch (e) {
+                console.log(`Error : ${e}`)
+        }
+}
+
+exports.fetchJson = fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+fetch(url, options).then(response => response.json())
+ .then(json => {
+//console.log(json)
+resolve(json)
+}).catch((err) => {
+reject(err)
+})
+})
+
+module.exports = { getBuffer}
