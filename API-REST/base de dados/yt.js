6b12533ce3f2d2ba2bb9049a6799bffe3a37e195
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-22 12:01:37 -0600

    Add/Update API-REST/base de dados/yt.js

---
+++ API-REST/base de dados/yt.js
@@ -0,0 +1,285 @@
+const yt = require("ytdl-core")
+const yts = require("yt-search")
+
+async function ytMp3(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const id = yt.getVideoID(url)
+      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+      .then((data) => {
+        let pormat = data.formats
+        let audio = []
+        for (let i = 0; i < pormat.length; i++) {
+          if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
+            let aud = pormat[i]
+            audio.push(aud.url)
+          }
+        }
+        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+        
+        const result = {
+          titulo: title,
+          thumb: thumb,
+          canal: channel,
+          publicado: published,
+          visu: views,
+          url: audio[1]
+        }
+        return(result)
+      })
+      resolve(yutub)
+    } catch (error) {
+        reject(error);
+      }
+      console.log(error)
+  })
+}
+
+async function ytMp4(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const id = yt.getVideoID(url)
+      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+      .then((data) => {
+        let pormat = data.formats
+        let video = []
+        for (let i = 0; i < pormat.length; i++) {
+          if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
+            let vid = pormat[i]
+            video.push(vid.url)
+          }
+        }
+        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+        
+        const result = {
+          titulo: title,
+          thumb: thumb,
+          canal: channel,
+          publicado: published,
+          visu: views,
+          url: video[0]
+        }
+        return(result)
+      })
+      resolve(yutub)
+    } catch (error) {
+        reject(error);
+      }
+      console.log(error)
+  })
+}
+
+async function ytPlay(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const search = yts(query)
+            .then((data) => {
+                const url = []
+                const pormat = data.all
+                for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].type == 'video') {
+                        let dapet = pormat[i]
+                        url.push(dapet.url)
+                    }
+                }
+                const id = yt.getVideoID(url[0])
+                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+                .then((data) => {
+                    let pormat = data.formats
+                    let audio = []
+                    let video = []
+                    for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
+                        let aud = pormat[i]
+                        audio.push(aud.url)
+                    }
+                    }
+                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+                    const result = {
+                    status: true,
+                    código: 200,
+                    criador: '@Kauanzin </>',
+                    titulo: title,
+                    thumb: thumb,
+                    canal: channel,
+                    publicado: published,
+                    visu: views,
+                    url: audio[0]
+                    }
+                    return(result)
+                })
+                return(yutub)
+            })
+            resolve(search)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+async function ytPlayMp4(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const search = yts(query)
+            .then((data) => {
+                const url = []
+                const pormat = data.all
+                for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].type == 'video') {
+                        let dapet = pormat[i]
+                        url.push(dapet.url)
+                    }
+                }
+                const id = yt.getVideoID(url[0])
+                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+                .then((data) => {
+                    let pormat = data.formats
+                    let video = []
+                    for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
+                        let vid = pormat[i]
+                        video.push(vid.url)
+                    }
+                   }
+                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+                    const result = {
+                    titulo: title,
+                    thumb: thumb,
+                    canal: channel,
+                    publicado: published,
+                    visu: views,
+                    url: video[0]
+                    }
+                    return(result)
+                })
+                return(yutub)
+            })
+            resolve(search)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+async function ytplay5(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const search = yts(query)
+            .then((data) => {
+                const url = []
+                const pormat = data.all
+                for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].type == 'audio') {
+                        let dapet = pormat[i]
+                        url.push(dapet.url)
+                    }
+                }
+                const id = yt.getVideoID(url[0])
+                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+                .then((data) => {
+                    let pormat = data.formats
+                    let audio = []
+                    for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
+                        let vid = pormat[i]
+                        audio.push(vid.url)
+                    }
+                   }
+                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+                    const result = {
+                    titulo: title,
+                    thumb: thumb,
+                    canal: channel,
+                    publicado: published,
+                    visu: views,
+                    url: audio[0]
+                    }
+                    return(result)
+                })
+                return(yutub)
+            })
+            resolve(search)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+async function ytSearch(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const cari = yts(query)
+            .then((data) => {
+                res = data.all
+                return res
+            })
+            resolve(cari)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+async function ytSearch(q) {
+return new Promise((resolve, reject) => {
+try {
+const cari = yts(q).then((data) => {
+res = data.all
+return res })
+resolve(cari)
+} catch (error) {
+reject(error)
+}
+console.log(error)
+})
+}
+
+async function ytVideosSearch(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const cari = yts(query)
+            .then((data) => {
+                res = data.videos
+                return res
+            })
+            resolve(cari)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+module.exports = {
+ytMp3,
+ytMp4,
+ytPlay,
+ytPlayMp4,
+ytSearch, 
+ytplay5,
+ytVideosSearch
+ };
