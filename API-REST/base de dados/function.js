d479e25d20bd688eff6a89e6c63716df64dcdd0e
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-09-22 12:01:59 -0600

    Add/Update API-REST/base de dados/function.js

---
+++ API-REST/base de dados/function.js
@@ -0,0 +1,74 @@
+const fs = require('fs-extra');
+const crypto = require('crypto');
+const pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ23456789'.split('');
+const request = require('request')
+
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256');
+    const hash = sha256.update(password).digest('base64');
+    return hash;
+}
+
+const generateAuthToken = () => {
+    return crypto.randomBytes(30).toString('hex');
+}
+
+function randomText(len) {
+    const result = [];
+    for (let i = 0; i < len; i++) result.push(pool[Math.floor(Math.random() * pool.length)]);
+    return result.join('');
+}
+
+function readFileTxt(file) {
+    return new Promise((resolve, reject) => {
+        const data = fs.readFileSync(file, 'utf8');
+        const array = data.toString().split('\n') ;
+        const random = array[Math.floor(Math.random() * array.length)];
+        resolve(random.replace('\r', ''));
+    })
+}
+
+function readFileJson(file) {
+    return new Promise((resolve, reject) => {
+        const jsonData = JSON.parse(fs.readFileSync(file));
+        const index = Math.floor(Math.random() * jsonData.length);
+        const random = jsonData[index];
+        resolve(random);
+    })
+}
+
+function ignoreFavicon(req, res, next) {
+    if (req.originalUrl.includes('favicon')) {
+      res.status(204).end()
+    }
+    next();
+}
+
+const getBuffer = async (url, options) => {
+        try {
+                options ? options : {}
+                const res = await axios({
+                        method: "get",
+                        url,
+                        headers: {
+                                'DNT': 1,
+                                'Upgrade-Insecure-Request': 1
+                        },
+                        ...options,
+                        responseType: 'arraybuffer'
+                })
+                return res.data
+        } catch (e) {
+                console.log(`Error : ${e}`)
+        }
+}
+
+
+
+const download_Url = function(uri, filename, callback){
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+};
+
+
+module.exports = { readFileTxt, readFileJson, getHashedPassword, generateAuthToken, randomText,
+    ignoreFavicon, download_Url, getBuffer };
