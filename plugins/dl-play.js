d1cef02dc12eb59d63a3043bdae29e0ebac0e78a
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-11-09 15:37:26 -0600

    fix a descargas de YouTube

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,8 +1,11 @@
+import yts from 'yt-search' 
+import { Ytdl } from './lib/Ytdl-Fz.js'
 const handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `\`\`\`[ 🌴 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did i tell u that i miss you\`\`\``;
 
     const randomReduction = Math.floor(Math.random() * 5) + 1;
     let search = await yts(text);
+    let f = `\n\n${String.fromCharCode(68,101,118,101,108,111,112,101,100,32,98,121,32,73,39,109,32,70,122,32,126)}`;
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
     let body = `\`\`\`⊜─⌈ 📻 ◜YouTube Play◞ 📻 ⌋─⊜
@@ -17,13 +20,13 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     
     conn.sendMessage(m.chat, { 
         image: { url: search.all[0].thumbnail }, 
-        caption: body 
+        caption: body + f
     }, { quoted: fkontak });
 
-    let res = await dl_vid(urls)
+    let res = isVideo ? await Ytdl.mp4(urls) : await Ytdl.mp3(urls)
     let type = isVideo ? 'video' : 'audio';
-    let video = res.data.mp4;
-    let audio = res.data.mp3;
+    let video = res.media
+    let audio = res.media
     conn.sendMessage(m.chat, { 
         [type]: { url: isVideo ? video : audio }, 
         gifPlayback: false, 
@@ -34,25 +37,5 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.command = ['play', 'playvid'];
 handler.help = ['play', 'playvid'];
 handler.tags = ['dl'];
-export default handler;
-
-async function dl_vid(url) {
-    const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
-        method: 'POST',
-        headers: {
-            'accept': '*/*',
-            'api_key': 'free',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            text: url
-        })
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-}
+handler.diamond = 4
+export default handler
