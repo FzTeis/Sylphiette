caadf9f61902e6243f29688ab2eec5e8dfbf1bff
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-02-08 02:05:48 -0600

    Eliminando owner-leave.js

--- owner-leave.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m, {conn, text, command}) => {
-  const id = text ? text : m.chat;
-  await conn.reply(id, '*🤓☝️*');
-  await conn.groupLeave(id);
-};
-handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i;
-handler.group = true;
-handler.rowner = true;
-export default handler;
