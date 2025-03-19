86b88c450523c16c6d25aaf0aab1c20cc54a5762
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-03-19 16:06:05 -0600

    Delete plugins/tools-toimg.js

--- plugins/tools-toimg.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!m.quoted) throw `✳️ ${mssg.replyStick}`
-    let xx = m.quoted;
-  conn.sendMessage(m.chat, { image: await xx.download(), caption: `\`\`\`[ 🌱 ] ${mssg.result}\`\`\`` }, m)
-        }
-handler.help = ['toimg <sticker>']
-handler.tags = ['sticker']
-handler.command = ['toimg', 'jpg', 'aimg'] 
-export default handler
