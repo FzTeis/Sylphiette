430875c710ddea6a76dce31b610d277a9b3b3980
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-03-19 15:55:02 -0600

    Create file dl-tiktok.js

---
+++ dl-tiktok.js
@@ -0,0 +1,38 @@
+import fg from 'api-dylux';
+
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+    try {
+        if (!args[0]) {
+            return conn.reply(m.chat, `✳️ Debes ingresar un enlace de TikTok.\n\n📌 *Ejemplo:* ${usedPrefix + command} https://vm.tiktok.com/ZMreHF2dC/`, m);
+        }
+
+        if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) {
+            return conn.reply(m.chat, `❎ Enlace de TikTok inválido.`, m);
+        }
+
+        m.react('🕒');
+
+        let data = await fg.tiktok(`${args[0]}`);
+        let { title, play, duration } = data.result;
+        let { nickname } = data.result.author;
+
+        let caption = `
+  乂 TikTok Download
+
+  ◦ 👤 *Autor:* ${nickname}
+  ◦ 📌 *Título:* ${title}
+  ◦ ⏱️ *Duración:* ${duration}`;
+
+        await conn.sendFile(m.chat, play, `tiktok.mp4`, caption, m);
+
+        m.react('✅');
+    } catch (e) {
+        return conn.reply(m.chat, `❌ *Error:* ${e.message}`, m);
+    }
+};
+
+handler.help = ["tiktok"];
+handler.tags = ["dl"];
+handler.command = ["tt", "tiktok", "ttdl"];
+
+export default handler;
