536b32b28df1232f98cffadfe222375610dfa1be
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-03-25 14:35:52 -0600

    Fix

---
+++ plugins/_premium-expired.js
@@ -0,0 +1,16 @@
+const handler = (m) => m;
+
+export async function all(m) {
+  for (const user of Object.values(global.db.data.users)) {
+    if (user.premiumTime != 0 && user.premium) {
+      if (new Date() * 1 >= user.premiumTime) {
+        user.premiumTime = 0;
+        user.premium = false;
+        const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user);
+        const usuarioJid = JID.split`@`[0];
+        const textoo = `🌳 Se agotó tu tiempo como usuario premium. Compra más usando !buyprem`;
+        await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: fkontak });
+      }
+    }
+  }
+}
