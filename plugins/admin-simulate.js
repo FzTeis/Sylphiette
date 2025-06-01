1e23a34009523473da870e4f15643ddb127e6604
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-05-31 19:20:36 -0600

    ¡Sylphy v2 ( Beta )!

---
+++ plugins/admin-simulate.js
@@ -0,0 +1,64 @@
+let handler = async (m, { conn, usedPrefix, command, args: [event], text }) => {
+
+  let chat = global.db.data.chats[m.chat]
+  if (!chat.welcome) throw `✳️ Para usar este comando debe actvar las Bienvenidas con\n\n *${usedPrefix}on* welcome`
+  let te = `
+## Lista de eventos :
+
+  - welcome
+  - bye
+  - promote
+  - demote
+
+  📌 Ejemplo :
+  
+  *${usedPrefix + command}* welcome @user`
+
+if (!event) return await m.reply(te) 
+
+let mentions = text.replace(event, '').trimStart()
+let who = mentions ? conn.parseMention(mentions) : []
+let part = who.length ? who : [m.sender]
+let act = false
+m.reply(`✅ Simulando ${event}...`)
+switch (event.toLowerCase()) {
+        case 'add':
+        case 'bienvenida':
+        case 'invite':
+        case 'welcome':
+           act = 'add'
+         break 
+        case 'bye':
+        case 'despedida':
+        case 'leave':
+        case 'remove':
+         act = 'remove'
+        break
+
+        case 'promote':
+        case 'promover':
+          act = 'promote'
+        break
+
+        case 'demote':
+        case 'degradar':
+         act = 'demote'
+        break
+
+default:
+
+throw te
+}
+if (act) return conn.participantsUpdate({
+id: m.chat,
+participants: part,
+action: act
+})
+}
+handler.help = ['simulate <event> @user']
+handler.tags = ['group']
+handler.command = ['simular', 'simulate'] 
+handler.admin = true
+handler.group = true
+
+export default handler
